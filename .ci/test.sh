f95e21f28006f153fb951abc3addc52c70246ef0
Mohammed Sheikh <Mohammed.Sheikh1@ibm.com>
2026-06-09 13:51:52 +0530

    skipped arrow test

--- .ci/test.sh
+++ .ci/test.sh
@@ -77,6 +77,8 @@ if [[ "$TASK" == "cpp-tests" ]]; then
             cmake_args+=("-DENABLED_SANITIZERS=$SANITIZERS")
         fi
     fi
+    if [[ -n "${EXTRA_CMAKE_ARGS:-}" ]]; then
+        cmake_args+=($EXTRA_CMAKE_ARGS)
     cmake -B build -S . "${cmake_args[@]}"
     cmake --build build --target testlightgbm -j4 || exit 1
     ./testlightgbm || exit 1
